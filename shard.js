e99f380740b1a02d9023c140c95036536303f3d4
SharifPoetra <43358217+SharifPoetra@users.noreply.github.com>
2018-10-03 06:35:43 +0800

    Update shard.js

--- shard.js
+++ shard.js
@@ -1,7 +1,7 @@
 const { ShardingManager } = require('discord.js');
 
 const Manager = new ShardingManager('./index.js', {
-  totalShards: 'auto', 
+  totalShards: 3, 
   token: process.env.TOKEN
 });
 
