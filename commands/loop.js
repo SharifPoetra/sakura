3c4d8f0e9ed0721ccf4151a1aa9eadad6f008545
SharifPoetra <43358217+SharifPoetra@users.noreply.github.com>
2018-10-05 20:00:14 +0800

    Update loop.js

--- commands/loop.js
+++ commands/loop.js
@@ -3,6 +3,7 @@ const { queue } = require('../index.js');
 this.run = (client, msg ,args) => {
   const serverQueue = queue.get(msg.guild.id);
   if(!serverQueue) return msg.channel.send({ embed: { description: 'Not playing anything right now'}});
+  if(serverQueue.voiceChannel.id !== msg.member.voiceChannel.id) return msg.channel.send({ embed: { color: 0xf91d1d, description: `You must be in **${serverQueue.voiceChannel.name}** to change the current volume`}});
   serverQueue.loop = !serverQueue.loop;
   queue.set(msg.guild.id, serverQueue);
   if(serverQueue.loop) return msg.channel.send({ embed: { description: '🔁 Looping Current Song.'}});
