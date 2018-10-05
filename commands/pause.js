75e609413ca1066ddc35daa3e6c48311d17706d2
SharifPoetra <43358217+SharifPoetra@users.noreply.github.com>
2018-10-05 19:58:31 +0800

    Update pause.js

--- commands/pause.js
+++ commands/pause.js
@@ -7,7 +7,7 @@ exports.run = async (client, msg, args) => {
 			serverQueue.connection.dispatcher.pause();
 			return msg.channel.send({ embed: { description: '⏸ Paused the music for you!'}});
 		}
-   // if(serverQueue.voiceChannel.id !== msg.member.voiceChannel.id) return msg.channel.send({ embed: { color: 0xf91d1d, description: `You must be in **${serverQueue.voiceChannel.name}** to pause the song`}});	
+    if(serverQueue.voiceChannel.id !== msg.member.voiceChannel.id) return msg.channel.send({ embed: { color: 0xf91d1d, description: `You must be in **${serverQueue.voiceChannel.name}** to pause the song`}});	
 		return msg.channel.send({ embed: { description: 'There is nothing playing.'}});
 } 
 
