ede692f77bbdc2bbcca2f9f3a4272c1cf42d1d33
SharifPoetra <43358217+SharifPoetra@users.noreply.github.com>
2018-10-05 19:37:39 +0800

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -4,11 +4,11 @@ const { RichEmbed } = require('discord.js');
 exports.run = async (client, msg, args) => {
 	const searchString = args.slice(1).join(' ');
 	const url = args[1] ? args[1].replace(/<(.+)>/g, '$1') : '';
-	const serverQueue = require('../index.js').queue.get(msg.guild.id);
+	
 	const voiceChannel = msg.member.voiceChannel;
 	if (!voiceChannel) return msg.channel.send({ embed: { description: 'I\'m sorry but you need to be in a voice channel to play music!'}});
 	if (!args[1]) return msg.channel.send({ embed: { color: 0x4286f4, description: `*Correct usage is*: **${prefix}play** ***[Song Name]/[Video URL]/[Playlist URL]***`}});
-	if(serverQueue.voiceChannel.id !== msg.member.voiceChannel.id) return msg.channel.send({ embed: { color: 0xf91d1d, description: `Woop I already playing in the other channel you must be in **${serverQueue.voiceChannel.name}** to request the song`}});	
+        //if(serverQueue.voiceChannel.id !== msg.member.voiceChannel.id) return msg.channel.send({ embed: { color: 0xf91d1d, description: `Woop I already playing in the other channel you must be in **${serverQueue.voiceChannel.name}** to request the song`}});	
 	const permissions = voiceChannel.permissionsFor(msg.client.user);
 	if (!permissions.has('CONNECT')) return msg.channel.send({ embed: { description: 'I cannot connect to your voice channel, make sure I have the proper permissions!'}});
 	if (!permissions.has('SPEAK')) return msg.channel.send({ embed: { description: 'I cannot speak in this voice channel, make sure I have the proper permissions!'}});
